d4be86de393327a50547853932383e4a8cd31571
AdrianGerman <adrian.german1019@gmail.com>
2024-05-07 08:19:55 -0600

    reflect

--- minirevi.js
+++ minirevi.js
@@ -4,13 +4,15 @@ class MiniReviReactive {
 
     this.$data = new Proxy(origen, {
       get(target, name) {
-        if (name in target) {
-          return target[name];
+        if (Reflect.has(target, name)) {
+          return Reflect.get(target, name);
         }
         console.warn("La propiedad que intentas acceder no existe");
         return "";
       },
-      set() {},
+      set(target, name, value) {
+        Reflect.set(target, name, value);
+      },
     });
 
     this.mount();
@@ -20,11 +22,21 @@ class MiniReviReactive {
     document.querySelectorAll("*[p-text]").forEach((el) => {
       this.pText(el, this.$data, el.getAttribute("p-text"));
     });
+    document.querySelectorAll("*[p-model]").forEach((el) => {
+      const name = el.getAttribute("p-model");
+      this.pModel(el, this.$data, name);
+
+      el.addEventListener("input", () => {
+        Reflect.set(this.$data, name, el.value);
+      });
+    });
   }
 
   pText(el, target, name) {
-    const content = target[name];
-    el.innerText = content;
+    el.innerText = Reflect.get(target, name);
+  }
+  pModel(el, target, name) {
+    el.value = Reflect.get(target, name);
   }
 }
 
