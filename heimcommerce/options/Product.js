817d024db8297dbb17dfcaeced7d342392da87d3
AdrianGerman <adrian.german1019@gmail.com>
2024-05-10 11:50:42 -0600

    watchers with options API

--- heimcommerce/options/Product.js
+++ heimcommerce/options/Product.js
@@ -22,7 +22,7 @@ app.component("product", {
       Quedan pocas unidades!
     </p>
     <p class="description__status" v-else-if="product.stock === 1">Ultima unidad!</p>
-    <p class="description__price">
+    <p class="description__price" :style="{ color: price_color }">
       $ {{ new Intl.NumberFormat("es-MX").format(product.price) }}
     </p>
     <p class="description__content">
@@ -47,6 +47,7 @@ app.component("product", {
     return {
       activeImage: 0,
       discountCodes: ["GERMAN100", "ADRIGER20"],
+      price_color: "rgb(104, 104, 209)",
     };
   },
   methods: {
@@ -59,6 +60,16 @@ app.component("product", {
     },
     sendToCart() {
       this.$emit("sendtocart", this.product);
+    },
+  },
+  watch: {
+    activeImage(value, oldValue) {
+      console.log(value, oldValue);
+    },
+    "product.stock"(stock) {
+      if (stock <= 1) {
+        this.price_color = "rgb(188, 30, 67)";
+      }
     },
   },
 });
