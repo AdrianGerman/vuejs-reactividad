43f9fa8967428a52aa4b9aab9257790ca766a922
AdrianGerman <adrian.german1019@gmail.com>
2024-05-10 11:15:24 -0600

    custom events with composition API

--- heimcommerce/composition/Product.js
+++ heimcommerce/composition/Product.js
@@ -38,23 +38,20 @@ app.component("product", {
               @keyup.enter="applyDiscount($event)"
             />
           </div>
-          <button :disabled="product.stock === 0" @click="addToCart()">Agregar al carrito</button>
+          <button :disabled="product.stock === 0" @click="sendToCart()">Agregar al carrito</button>
         </section>
     `,
   props: ["product"],
-  setup(props) {
+  emits: ["sendtocart"],
+  setup(props, context) {
     const productState = reactive({
       activeImage: 0,
     });
-    function addToCart() {
-      const prodIndex = cartState.cart.findIndex((prod) => prod.name === props.product.name);
-      if (prodIndex >= 0) {
-        cartState.cart[prodIndex].quantity += 1;
-      } else {
-        cartState.cart.push(props.product);
-      }
-      props.product.stock -= 1;
+
+    function sendToCart() {
+      context.emit("sendtocart", props.product);
     }
+
     const discountCodes = ref(["GERMAN100", "ADRIGER20"]);
     function applyDiscount(event) {
       const discountCodeIndex = discountCodes.value.indexOf(event.target.value);
@@ -66,8 +63,8 @@ app.component("product", {
 
     return {
       ...toRefs(productState),
-      addToCart,
       applyDiscount,
+      sendToCart,
     };
   },
 });
